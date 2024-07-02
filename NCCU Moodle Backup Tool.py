864a6eef30268a4905f86af6e643b2ec1e588f80
chengmoxu <rolly200311@gmail.com>
2024-07-02 18:40:50 +0800

    Code optimization

--- NCCU Moodle Backup Tool.py
+++ NCCU Moodle Backup Tool.py
@@ -65,25 +65,26 @@ while UserEULAjudge == "":
 
 while UserEULAjudge == "Start":
     driver.get("https://i.nccu.edu.tw/Login.aspx?app=moodle&ReturnUrl=%2fsso_app%2fMoodleSSO.aspx")
+    print ("請在自動化視窗中輸入您的政大單一化登入窗口帳號密碼，並在跳轉完成後在此輸入1以繼續")
+    print ("或者，輸入0結束\n")
+    userinput = input("請輸入：")
     mode = ""
     while mode == "":
-        print ("請在自動化視窗中輸入您的政大單一化登入窗口帳號密碼，並在跳轉完成後在此輸入1以繼續")
-        print ("或者，輸入0結束\n")
-        userinput = input ('請輸入：')
         userinput_judge = str.isdigit(userinput)
         if userinput_judge == True: 
             if  userinput == "1":
-                mode = "1"
+                mode = "Continue"
                 break
             elif userinput == "0":
-                mode = "9"
+                mode = "Exit"
                 break
             else:
                 print ("請重新輸入正確選項！")
+                mode = ""
         elif userinput_judge == False:
             print ("請重新輸入正確選項！")
-    
-    while mode == "1":
+            mode = ""
+    while mode == "Continue":
         # Get Moodle MAain Page via find_elements
         course_elements = driver.find_elements(By.CSS_SELECTOR, ".content .unlist .column.c1 a")
         courses = {course.get_attribute("title"): course.get_attribute("href") for course in course_elements} # Name and Direct Link
@@ -139,5 +140,5 @@ while UserEULAjudge == "Start":
                             time.sleep(10)
         break
     #driver.quit()
-while UserEULAjudge == "Exit" or mode == 9:
+while UserEULAjudge == "Exit" or mode == "Exit":
     os.system('pause')
