6b8c81a389963703764eb27bd3b883f0a43222f9
chengmoxu <rolly200311@gmail.com>
2024-07-02 15:00:05 +0800

    Added the function of creating files and downloading attachments for the introductory text of courses from Week1 to Week16/18

--- NCCU Moodle Backup Tool.py
+++ NCCU Moodle Backup Tool.py
@@ -1,6 +1,6 @@
 # NCCU Moodle Backup Tool
-# Version: v0.0.1 Beta
-# Build: 20240630.1
+# Version: v0.0.2 Beta
+# Build: 20240702.1
 
 import os
 import time
@@ -8,9 +8,18 @@ import selenium
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from lxml import etree
 
 driver = webdriver.Edge()
 driver.get("https://i.nccu.edu.tw/Login.aspx?app=moodle&ReturnUrl=%2fsso_app%2fMoodleSSO.aspx")
+
+edge_options = webdriver.EdgeOptions()
+prefs = {
+    "profile.default_content_settings.popups": 0,
+    "directory_upgrade": True,
+    "safebrowsing.enabled": True
+}
+edge_options.add_experimental_option("prefs", prefs)
 
 mode = ""
 while mode == "":
@@ -30,8 +39,9 @@ while mode == "":
     elif userinput_judge == False:
         print ("請重新輸入正確選項！")
 while mode == "1":
+    # Get Moodle MAain Page via find_elements
     course_elements = driver.find_elements(By.CSS_SELECTOR, ".content .unlist .column.c1 a")
-    courses = {course.get_attribute("title"): course.get_attribute("href") for course in course_elements}
+    courses = {course.get_attribute("title"): course.get_attribute("href") for course in course_elements} # Name and Direct Link
     for course_name, course_link in courses.items():
         # 去除不合規則的文件名字元
         valid_course_name = "".join(char for char in course_name if char.isalnum() or char in " _-")
@@ -40,7 +50,48 @@ while mode == "1":
         driver.get(course_link)
         time.sleep(2)  # 等待頁面載入完成
         # section-0 = 公告/課程大綱
+        section0 = driver.find_element(By.CSS_SELECTOR, '#section-0')
+        folder_name = "課程大綱及公告"
+
         # section-1至# section-18 = 課程內容
+        #判斷16/18週
+        if driver.find_element (By.CSS_SELECTOR, '#section-16') == True:
+            if driver.find_element (By.CSS_SELECTOR, '#section-18') == True:
+                sx = 1
+                for sx in range(1,18,1):
+                    sx_css_selector_con = str("#section-"+sx)
+                    sectionX= driver.find_element (By.CSS_SELECTOR, sx_css_selector_con)
+                    folder_name = sectionX.get_attribute ('aria-label')
+                    content_element = sectionX.find_element (By.CSS_SELECTOR, '.no-overflow')
+                    content = content_element.get_attribute ('innerHTML')
+                    if not os.path.exists(folder_name):
+                        os.makedirs(folder_name)
+                    file_path = os.path.join(folder_name, "介紹.txt")
+                    with open(file_path, "w", encoding="utf-8") as file:
+                        file.write(content)
+                    course_resource_links = sectionX.find_elements(By.CSS_SELECTOR, '.activityinstance a')
+                    for link in course_resource_links:
+                        link_url = link.get_attribute('href')
+                        driver.get(link_url)
+                        time.sleep(10)
+            else:
+                sx = 1
+                for sx in range(1,16,1):
+                    sx_css_selector_con = str("#section-"+sx)
+                    sectionX= driver.find_element (By.CSS_SELECTOR, sx_css_selector_con)
+                    folder_name = sectionX.get_attribute ('aria-label')
+                    content_element = sectionX.find_element (By.CSS_SELECTOR, '.no-overflow')
+                    content = content_element.get_attribute ('innerHTML')
+                    if not os.path.exists(folder_name):
+                        os.makedirs(folder_name)
+                    file_path = os.path.join(folder_name, "介紹.txt")
+                    with open(file_path, "w", encoding="utf-8") as file:
+                        file.write(content)
+                    course_resource_links = sectionX.find_elements(By.CSS_SELECTOR, '.activityinstance a')
+                    for link in course_resource_links:
+                        link_url = link.get_attribute('href')
+                        driver.get(link_url)
+                        time.sleep(10)
     break
 #driver.quit()
 while mode == "9":
